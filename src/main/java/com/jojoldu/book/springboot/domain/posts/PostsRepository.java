d49fb8164da813b12ab528f70b8aae77d161c7de
Beginner <dkstmdwn17@naver.com>
2022-01-17 16:15:55 +0900

    Update, Delete 작업 추가

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,14 @@
 package com.jojoldu.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
 // 보통 ibatis, mybatis 등에서 DAO라고 불리는 DB Layer 접근자
 // JPA에서는 Repository라고 부르며 인터페이스로 생성합니다.
 // 단순히 인터페이스를 생성 후, JpaRepository<Entity클래스, PK타입>을 상속하면 기본적인 CRUD 메서드가 자동으로 생성
 public interface PostsRepository extends JpaRepository<Posts,Long> {
+    @Query("Select p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/jojoldu/book/springboot/web/domain/posts/PostsRepositoryTest.java
@@ -50,7 +50,7 @@ public class PostsRepositoryTest {
     @Test
     public void BaseTimeEntity_등록(){
         // given
-        LocalDateTime now = LocalDateTime.of(2019,6,4,0,0,0);
+        LocalDateTime now = LocalDateTime.of(2022,1,17,0,0,0);
         postsRepository.save(Posts.builder()
                 .title("title")
                 .content("content")
